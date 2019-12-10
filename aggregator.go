b2cb7213af7cdf43ca29028b499d371c27663edf
Bo Maryniuk <bo@suse.de>
2019-12-10 23:41:23 +0100

    Add ID remap

--- aggregator.go
+++ aggregator.go
@@ -1,7 +1,7 @@
 package uyuniapi
 
 import (
-	"github.com/davecgh/go-spew/spew"
+	"reflect"
 )
 
 type DataAggregator struct {
@@ -25,16 +25,43 @@ func (agr *DataAggregator) aggregate(fqdn string, data interface{}) {
 
 // Multiplex data from different sources into one
 func (agr *DataAggregator) Multiplex() interface{} {
-	data := make([]interface{}, 0)
+	data, merged := agr.mergeStructs()
+	if !merged {
+		data = nil
+	}
+
 	return data
 }
 
 func (agr *DataAggregator) mergeStructs() (interface{}, bool) {
-	merged := false
+	var data interface{} = nil
 	if len(agr.shards) > 0 {
-		//if reflect.TypeOf(agr.vidman.GetContextFQDNs()[0]) ==
-		spew.Dump(agr.vidman.GetContextFQDNs()[0])
+		for _, rpcRef := range agr.vidman.GetContextFQDNs() {
+			res := agr.shards[rpcRef]
+			if reflect.TypeOf(res).Kind() == reflect.Slice {
+				if data == nil {
+					data = make([]interface{}, 0)
+				}
+
+				for _, dataRef := range res.([]interface{}) {
+					if reflect.TypeOf(dataRef).Kind() == reflect.Map {
+						data = append(data.([]interface{}), agr.remapIds(rpcRef, dataRef))
+					}
+				}
+			}
+		}
 	}
 
-	return nil, merged
+	return data, data != nil
+}
+
+// Remap system IDs
+func (agr *DataAggregator) remapIds(context string, data interface{}) interface{} {
+	var rData interface{} = data
+	if reflect.TypeOf(data).Kind() == reflect.Map {
+		if systemId, ok := data.(map[string]interface{})["id"]; ok { // TODO: check if it is really string/interface map
+			data.(map[string]interface{})["id"] = agr.vidman.GetContext(context).ToVirtualId(int(systemId.(int64)))
+		}
+	}
+	return rData
 }
