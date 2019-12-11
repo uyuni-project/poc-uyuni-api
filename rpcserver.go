4f2edad71f7090f75bc0fb0ac8f441f5ced53b60
Bo Maryniuk <bo@suse.de>
2019-12-11 18:12:09 +0100

    Add RPC server (initial)

---
+++ rpcserver.go
@@ -0,0 +1,52 @@
+package uyuniapi
+
+import "github.com/gin-gonic/gin"
+
+type RPCServer struct {
+	vdm    *VIDManager
+	mux    *RPCDemux
+	router *gin.Engine
+	setup  bool
+}
+
+// Constructor
+func NewRPCServer() *RPCServer {
+	srv := new(RPCServer)
+	srv.vdm = NewVIDManager()
+	srv.mux = NewRPCDemux(srv.vdm)
+	gin.SetMode(gin.DebugMode)
+	srv.router = gin.New()
+	srv.setup = false
+
+	return srv
+}
+
+func (srv *RPCServer) AddHandler(uri string, handler gin.HandlerFunc) {
+	srv.router.Any(uri, handler)
+}
+
+// Setup is to update configuration of the server
+func (srv *RPCServer) Setup(config interface{}) *RPCServer {
+	if !srv.setup {
+		// Add sources
+		srv.vdm.AddContext("localhost")
+
+		// Basic router middleware setup
+		srv.router.Use(gin.Logger())
+		srv.router.Use(gin.Recovery())
+		srv.setup = true
+	}
+
+	return srv
+}
+
+func (srv *RPCServer) Start() {
+	if !srv.setup {
+		panic("Setup first!")
+	}
+
+	err := srv.router.Run(":8080")
+	if err != nil {
+		panic(err)
+	}
+}
