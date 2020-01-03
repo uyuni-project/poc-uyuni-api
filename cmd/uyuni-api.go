5a871fa7cd31a407e646f507412ace1b6ba02774
Bo Maryniuk <bo@suse.de>
2020-01-03 17:30:36 +0100

    Add CLI support

--- cmd/uyuni-api.go
+++ cmd/uyuni-api.go
@@ -2,15 +2,48 @@ package main
 
 import (
 	"github.com/isbm/uyuni-api"
+	"github.com/urfave/cli/v2"
+	"log"
+	"os"
 )
 
-func main() {
+func contextRun(ctx *cli.Context) error {
 	hdl := uyuniapi.NewUyuniXMLRPCHandler()
-	hdl.SetMethodMap("mgr-api.spec.conf")
+	hdl.SetMethodMap(ctx.String("rpcspec"))
 
-	cfg := uyuniapi.NewAPIConfig("mgr-api.conf")
+	cfg := uyuniapi.NewAPIConfig(ctx.String("config"))
 
 	rpc := uyuniapi.NewRPCServer()
 	rpc.AddHandler(hdl)
 	rpc.Setup(cfg).Start()
+
+	return nil
+}
+
+func main() {
+	app := &cli.App{
+		Version: "0.1 Alpha",
+		Name:    "mgr-api",
+		Usage:   "Uyuni API Microgateway",
+		Action:  contextRun,
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "rpcspec",
+				Aliases: []string{"s"},
+				Value:   "/etc/rhn/mgr-api.spec.conf",
+				Usage:   "XML-RPC method specifications for Uyuni",
+			},
+			&cli.StringFlag{
+				Name:    "config",
+				Aliases: []string{"c"},
+				Value:   "/etc/rhn/mgr-api.conf",
+				Usage:   "full configuration of the gateway",
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
