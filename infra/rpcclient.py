7c0fe527e2e773afb04f5940791788ea30b5e886
Bo Maryniuk <bo@suse.de>
2020-01-13 14:28:36 +0100

    Drop XML-RPC examples

--- infra/rpcclient.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-import xmlrpc.client
-
-conn = xmlrpc.client.ServerProxy("http://localhost:8000/rpc/api")
-print(conn.systems.listSystems("token"))
-print(conn.systems.getDetails("token", 1000010000))
